089ae76f0a260ed52181b26e9a5066de8abd2ca5
Roger2500 <rj.m.m.2500@estudiantec.cr>
2022-08-08 15:55:39 -0600

    Changes to testbench generator and improved the Dataset generator

---
+++ data_generator.py
@@ -0,0 +1,107 @@
+'''
+Data_Generator:
+A set of random dataset generation functions for circuits
+
+'''
+import math
+from random import *
+import numpy as np
+
+
+def dataset_gen(file:str, inputs: list, samples :int, distribution='uniform',**kwargs):
+    '''
+
+    Generates a file with with columns of random data, corresponding to n-bits circuit inputs.
+
+    :param file: File to save the dataset.
+    :param inputs: A list of number of bits for each input. For each number n in the list,
+                   a column of n bits numbers will be generated, corresponding to each input.
+    :param samples: How many samples of input data to generate.
+    :param distribution: Desired distribution of probability for the random data.
+                         could be: 'uniform'(default), 'normal','triangular',...
+    :param kwargs: Additional parameters for uniform distributions. For example:
+                   mean: Data mean for normal distribution. Behaves as peak for some distributions.
+                   variance: Data variance for the normal distribution.
+                   min: minimum allowed number. Behaves as lower threshold for some distributions.
+                   max: maximum allowed number. Behaves as upper threshold for some distributions.
+    :return: path to generated file
+    '''
+    text=''
+    data=[]
+    for n in inputs:
+        data.append(get_random(n,distribution,samples,**kwargs))
+    data=np.transpose(data)
+    file=np.savetxt(file, data, fmt='%d')
+
+    return file
+
+def get_random(bits: int, distribution='uniform', samples=1, **kwargs):
+    '''
+
+    Generates a random number according to a probability distribution using inverse CDF.
+
+    :param bits: Number of bits for generated data.
+    :param distribution: Desired distribution of probability for the random data.
+                         could be: 'uniform'(default), 'normal','triangular',...
+    :param samples: How many data samples to generate. Default:1
+    :param kwargs: Additional parameters for uniform distributions. For example:
+                   median: Data median for normal distribution. Behaves as peak for some distributions.
+                   variance: Data variance for the normal distribution.
+                   low_limit: minimum allowed number. Behaves as lower threshold for some distributions.
+                   high_limit: maximum allowed number. Behaves as upper threshold for some distributions.
+
+    :return: a random integer that follows the given distribution
+    '''
+
+    '''Pasing kwargs'''
+    if not 'low_limit' in kwargs:
+        low_limit=0 #Lower threshold for generated numbers
+    else:
+        low_limit=np.min([kwargs['low_limit'],2**bits])
+    if not 'high_limit' in kwargs:
+        high_limit=2**bits #Upper threshold for the generated data
+    else:
+        high_limit=np.min([kwargs['high_limit'],2**bits])
+    if not 'median' in kwargs:
+        median=(high_limit+low_limit)/2
+    else:
+        median=kwargs['median']
+    if not 'variance' in kwargs:
+        variance=1
+    else:
+        variance=kwargs['variance']
+
+    '''Distributions case'''
+    data=[]
+    if distribution in {'uniform', 'rectangular'}:
+        data=[int(math.floor(uniform(low_limit,high_limit))) for i in range(samples)]
+    elif distribution=='triangular':
+        data=[int(math.floor(triangular(low_limit,high_limit,mode=median))) for i in range(samples)]
+    elif distribution in {'normal', 'gaussian'}:
+        while len(data)<samples:
+            i=int(math.floor(gauss(median,variance)))
+            if low_limit<=i<=high_limit: data.append(i)
+    else:
+        raise ValueError(f'{distribution} is not a valid distribution name')
+
+    return data
+
+# from matplotlib import pyplot as plt
+# from scipy.interpolate import UnivariateSpline
+# path='/home/roger/Desktop/Electronica/ALS-benchmark-circuits/sample.txt'
+# dataset_gen(path,[1,2,3,4,5,6,7,8],samples=100000,distribution='gaussian', variance=10)
+#
+# data=np.loadtxt(path)
+# data=np.transpose(data)
+#
+#
+#
+# N=1000
+# n=N//1
+# for d in data:
+#     p,x=np.histogram(d,bins=n)
+#     x=x[:-1]+(x[1]-x[0])/2
+#     f=UnivariateSpline(x,p,s=n)
+#     plt.plot(x,f(x))
+#     plt.show()
+#
--- tb_generator.py
+++ tb_generator.py
@@ -1,88 +1,106 @@
 import os.path
-import random
 import sys
-iter_num=100_000;
-def dataset_gen(path:str, inputs: list, length :int):
-    text=''
-    for r in range(length):
-        for n in inputs:
-            n=10
-            text=text+str(random.getrandbits(n)) + ' '
-        text=text+'\n'
+from data_generator import *
 
-    with open(path,"w") as file:
-        file.write(text)
-        file.close
+iter_num=1_000_000; #How iterations (outputs, data readings...)
 
-def tb_generator(name: str, path:str, inputs_length, outputs_length):
+def tb_generator(topmodule: str, path:str, inputs_length, outputs_length, samples, dump_vcd=False):
+    '''
+    Writes a general testbench .v file for a circuit. Assumes a dataset in the same directory,
+    generated with data_generator.py script.
+
+    Dataset must be a file named 'dataset' (without extension).
+    The output will be a txt file named 'output.txt'
+
+    :param topmodule: string
+        Name of the circuit's top module
+    :param path: path-like string
+        Where to save the testbench. File's name is formatted as {topmodule}_tb.v.
+    :param inputs_length: integer list
+        A list of bitwidth for each input in the circuit. The length of the list tells how many inputs
+        does the circuit have, each element indicates how many bits the input has.
+    :param outputs_length: integer list
+        A list of bitwidth for each output in the circuit. The length of the list tells how many inputs
+        does the circuit have, each element indicates how many bits the output has.
+    :param samples: integer
+        How many samples the input dataset has.
+    :param dump_vcd: boolean
+        Whether to dump or not the wave information into a vcd file.
 
 
+    :return: path-like string
+        Path to the generated testbench file.
+    '''
 
-    text= '`timescale 1ns / 1ps \n' \
-        '\n' \
-        'module ' + name + '_tb(); \n' \
-        '\n'
+    text= f'`timescale 1ns / 1ps \n' \
+          f'\n' \
+          f'module {topmodule}_tb(); \n' \
+          f'\n'
     i=0
     inputs=[]
     outputs=[]
     for l in outputs_length:
         if l==1:
-            text= text+' wire out'+str(i)+';\n'
+            text= f'{text}wire out{i};\n'
         else:
-            text= text+' wire ['+ str(l-1)+':0] out'+str(i)+';\n'
-        outputs.append('out'+str(i))
+            text= f'{text}wire [{l-1}:0] out{i};\n'
+        outputs.append(f'out {i}')
         i+=1
     i=0
     for l in inputs_length:
         if l==1:
-            text= text+' reg in'+str(i)+';\n'
+            text= f'{text} reg in{i};\n'
         else:
-            text= text+' reg ['+ str(l-1)+':0] in'+str(i)+';\n'
-        inputs.append('in'+str(i))
+            text= f'{text} reg [{l-1}:0] in{i};\n'
+        inputs.append(f'in {i}')
         i+=1
 
-    text= text+ '\n' \
-                ' integer i, file, mem, temp;\n' \
-                '\n' \
-                ' '+ name +' U0('
+    text= f'{text}\n' \
+          f'integer i, file, mem, temp;\n' \
+          f'\n' \
+          f'{topmodule} U0('
     for i in inputs:
-        text=text+i+','
+        text= f'{text}{i},'
     for o in outputs[0:-1]:
-        text=text+o+','
-    text=text+outputs[-1]+');\n' \
-                        '\n' \
-                        ' initial begin\n' \
-                        ' //$display("-- Begining Simulation --");\n' \
-                        '\n' + f' $dumpfile("./{name}.vcd");\n' \
-                        ' $dumpvars(0,'+name+'_tb);\n' \
-                        ' file=$fopen("output.txt","w");\n' \
-                        ' mem=$fopen("dataset", "r");\n'
+        text= f'{text}{o},'
+    text= f'{text}{outputs[-1]});\n' \
+          f'\n' \
+          f'initial begin\n'
+    if dump_vcd:
+        text=f'{text} //$display("-- Begining Simulation --");\n' \
+             f'\n' \
+             f' $dumpfile("./{topmodule}.vcd");\n' \
+             f' $dumpvars(0,{topmodule}_tb);\n'
+    text=f'{text} file=$fopen("output.txt","w");\n' \
+         f' mem=$fopen("dataset", "r");\n'
     for i in inputs:
-        text=text+ ' '+ i+' = 0;\n'
-    text=text+' #10\n' \
-              ' for (i=0;i<'+str(iter_num)+';i=i+1) begin\n' \
-              '  temp=$fscanf(mem,"'
+        text=f'{text} {i} = 0;\n'
+    text=f'{text} #10\n' \
+         f' for (i=0;i<{samples};i=i+1) begin\n' \
+         f'  temp=$fscanf(mem,"'
     for i in range(len(inputs)):
-        text=text+'%d '
-    text=text+'\\n"'
+        text=f'{text}%d '
+    text=f'{text}\\n"'
     for i in inputs:
-        text=text+ ','+ str(i)
-    text=text+');\n' \
-              '  #10\n' \
-              '  $fwrite(file, "%d\\n",{'
+        text=f'{text},{i}'
+    text=f'{text});\n' \
+         f'  #10\n' \
+         f'  $fwrite(file, "%d\\n",'+'{'
     for o in outputs[::-1][0:-1]:
-        text=text+o+','
-    text=text+outputs[0]+'});\n' \
-                          '  end\n' \
-                          '  $fclose(file);\n' \
-                         '   $fclose(mem);\n' \
-                          '  //$display("-- Ending Simulation --");\n' \
-                          '  $finish;\n' \
-                          ' end\n' \
-                          'endmodule\n'
-    with open(os.path.join(path,name+'_tb.v'),'w') as file:
+        text= f'{text}{o},'
+    text= f'{text}{outputs[0]}'+'});\n'\
+        + f'  $display("--Progress: %d/{samples}--",i+1);\n'\
+          f' end\n' \
+          f' $fclose(file);\n' \
+          f' $fclose(mem);\n' \
+          f' $finish;\n' \
+          f'end\n' \
+          f'endmodule\n'
+    with open(os.path.join(path, topmodule + '_tb.v'), 'w') as file:
         file.write(text)
         file.close()
+
+    return os.path.join(path, topmodule + '_tb.v')
 
 def test_tb(name: str, path:str):
     kon= f'iverilog -o {path}/{name} {path}/{name}.v {path}/{name}_tb.v'
@@ -95,97 +113,97 @@ def test_tb(name: str, path:str):
 
 
 '''TEST'''
-# name=[
-# 'BK_16b',
-# 'BK_32b',
-# #Fir no incluido, tb propio secuencial# 'fir',
-# 'KS_16b',
-# 'KS_32b',
-# 'Mul_16b',
-# 'Mul_32b',
-# 'adder_128b',
-# 'barshift_128b',
-# 'div_64b', #Particularmente lento de simular
-# 'hyp_128b', #Particularmente lento de simular
-# 'log2_32b',
-# 'max_128b',
-# 'mul_64b',
-# 'sin_24b',
-# 'sqrt_128b',
-# 'square_64b',
-# 'CLA_16b',
-# 'CSkipA_16b',
-# 'LFA_16b',
-# 'RCA_4b',
-# 'WT_8b',
-# 'fwrdk2j',
-# 'voter',
-# 'int2float',
-# 'dec',
-# 'sobel'
-# ]
-# path=name
-# inputs=[
-# [16, 16],
-# [32, 32],
-# #Entradas de fir [8,2]
-# [16, 16],
-# [32, 32],
-# [16, 16],
-# [32, 32],
-# [128, 128],
-# [128, 7],
-# [64,64],
-# [128, 128],
-# [32],
-# [128, 128, 128, 128],
-# [64, 64],
-# [24],
-# [128],
-# [64],
-# [16, 16, 1],
-# [16, 16],
-# [16, 16],
-# [4, 4, 1],
-# [8, 8],
-# [32, 32],
-# [1001],
-# [11],
-# [8],
-# [9, 9, 9, 9, 9, 9, 9, 9, 9]
-# ]
-# outputs=[
-# [17],
-# [33],
-# #Salidas de FIR [10],
-# [17],
-# [33],
-# [32],
-# [64],
-# [128,1],
-# [128],
-# [64,64],
-# [128],
-# [32],
-# [128,2],
-# [128],
-# [25],
-# [64],
-# [128],
-# [17],
-# [17],
-# [17],
-# [5],
-# [16],
-# [32, 32],
-# [1],
-# [4,3],
-# [128, 128],
-# [8]
-# ]
-# for n,p,i,o in zip(name,path,inputs,outputs):
-#     print(f'Generating {n} testbench and dataset...')
-#     #dataset_gen(path=os.path.join(p,'dataset'),inputs=i,length=iter_num)
-#     #tb_generator(n,p,i,o)
-#     test_tb(n,p)
+name=[
+'BK_16b',
+'BK_32b',
+#Fir no incluido, tb propio secuencial# 'fir',
+'KS_16b',
+'KS_32b',
+'Mul_16b',
+'Mul_32b',
+'adder_128b',
+'barshift_128b',
+'div_64b', #Particularmente lento de simular
+'hyp_128b', #Particularmente lento de simular
+'log2_32b',
+'max_128b',
+'mul_64b',
+'sin_24b',
+'sqrt_128b',
+'square_64b',
+'CLA_16b',
+'CSkipA_16b',
+'LFA_16b',
+'RCA_4b',
+'WT_8b',
+'fwrdk2j',
+'voter',
+'int2float',
+'dec',
+'sobel'
+]
+path=name
+inputs=[
+[16, 16],
+[32, 32],
+#Entradas de fir [8,2]
+[16, 16],
+[32, 32],
+[16, 16],
+[32, 32],
+[128, 128],
+[128, 7],
+[64,64],
+[128, 128],
+[32],
+[128, 128, 128, 128],
+[64, 64],
+[24],
+[128],
+[64],
+[16, 16, 1],
+[16, 16],
+[16, 16],
+[4, 4, 1],
+[8, 8],
+[32, 32],
+[1001],
+[11],
+[8],
+[9, 9, 9, 9, 9, 9, 9, 9, 9]
+]
+outputs=[
+[17],
+[33],
+#Salidas de FIR [10],
+[17],
+[33],
+[32],
+[64],
+[128,1],
+[128],
+[64,64],
+[128],
+[32],
+[128,2],
+[128],
+[25],
+[64],
+[128],
+[17],
+[17],
+[17],
+[5],
+[16],
+[32, 32],
+[1],
+[4,3],
+[128, 128],
+[8]
+]
+for n,p,i,o in zip(name,path,inputs,outputs):
+    print(f'Generating {n} testbench and dataset...')
+    dataset_gen(file=os.path.join(p,'dataset'),inputs=i,samples=iter_num)
+    tb_generator(n,p,i,o, dump_vcd=False, samples=iter_num)
+    #test_tb(n,p)
 
